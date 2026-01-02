d5191a4e23ddadf86846fb78002be01a712ebdea
Samiran Royy <sroy21191@gmail.com>
2026-01-02 23:09:17 +0530

    in-progress

---
+++ hospitalManagement/src/main/java/com/example/hospitalManagement/Entity/Patient.java
@@ -0,0 +1,19 @@
+package com.example.hospitalManagement.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+    private  String name;
+    private LocalDate birthDate;
+    private String email;
+    private String gender;
+}
---
+++ hospitalManagement/src/main/java/com/example/hospitalManagement/repository/PatientRepository.java
@@ -0,0 +1,4 @@
+package com.example.hospitalManagement.repository;
+
+public interface PatientRepository {
+}
