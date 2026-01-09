83f04fad0e48111dbcb1bbada7c5e8db4d7978c5
Samiran Royy <sroy21191@gmail.com>
2026-01-09 22:01:54 +0530

    no message

--- hospitalManagement/src/main/java/com/example/hospitalManagement/HospitalManagementApplication.java
+++ hospitalManagement/src/main/java/com/example/hospitalManagement/HospitalManagementApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HospitalManagementApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HospitalManagementApplication.class, args);
 	}
 
