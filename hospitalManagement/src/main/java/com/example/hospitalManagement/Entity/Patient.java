209ef821b2a637e63fb12d75f491bc4e65ef7b31
Samiran Royy <sroy21191@gmail.com>
2026-02-16 20:34:57 +0530

    no message

--- hospitalManagement/src/main/java/com/example/hospitalManagement/Entity/Patient.java
+++ hospitalManagement/src/main/java/com/example/hospitalManagement/Entity/Patient.java
@@ -10,9 +10,9 @@ import java.time.LocalDateTime;
 @Entity
 @Table(
 
-        name = "patient_tbl", uniqueConstraints = {
+        name = "Patient", uniqueConstraints = {
         @UniqueConstraint(name = "unique_patient_tbl_email",columnNames = {"email"}),
-        @UniqueConstraint(name = "Unique_patient_tbl_name_birthDate", columnNames = {"name","birthDate"})
+        @UniqueConstraint(name = "Unique_patient_tbl_name_birthDate", columnNames = {"patient_name","birthDate"})
 },
         indexes = {
                 @Index(name = "idx_patient_name_birthDate",columnList = "birthDate")
@@ -23,13 +23,18 @@ public class Patient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  Long id;
-    @Column(name = "patient_name", nullable = false,length = 255,scale = )
+    @Column(name = "patient_name", nullable = false,length = 255)
     private  String name;
     @ToString.Exclude
+    @Column(name = "birthDate")
     private LocalDate birthDate;
     @Column(unique = true,nullable = false)
     private String email;
     private String gender;
+    @ToString.Exclude
+    private String Bloodgroup;
     @CreationTimestamp
+    @Column(updatable = false)
+//    updateable=false makes sure that in future this createdAt will never change.
     private LocalDateTime createdAt;
 }
---
+++ hospitalManagement/src/main/resources/Data.sql
@@ -0,0 +1,13 @@
+
+
+INSERT INTO patient(patient_name,gender,birthDate,email,Bloodgroup) VALUES
+('Ramesh Kumar','Male','1988-04-20','ramesh.kumar@example.com','B+'),
+ ('Priya Sharma','Female','1995-09-12','priya.sharma@example.com','O-'),
+ ('Arjun Patel','Male','1993-02-17','arjun.patel@example.com','A+'),
+ ('Anika Mehta','Female','1987-11-30','anika.mehta@example.com','AB-'),
+ ('Rahul Verma','Male','2001-06-05','rahul.verma@example.com','O+'),
+ ('Sonia Iyer','Female','1990-03-14','sonia.iyer@example.com','A-'),
+ ('Harish Reddy','Male','1982-08-09','harish.reddy@example.com','B-'),
+ ('Deepika Rao','Female','1994-07-22','deepika.rao@example.com','AB+'),
+ ('Vinayak Desai','Male','1997-10-10','vinayak.desai@example.com','O-'),
+('Neha Gupta','Female','1989-05-18','neha.gupta@example.com','A+');
