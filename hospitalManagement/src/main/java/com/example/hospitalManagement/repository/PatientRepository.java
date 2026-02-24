1e2302d11cf4226a1d85433a400d3d331431f372
Samiran Royy <sroy21191@gmail.com>
2026-02-24 19:52:54 +0530

    no message

--- hospitalManagement/src/main/java/com/example/hospitalManagement/repository/PatientRepository.java
+++ hospitalManagement/src/main/java/com/example/hospitalManagement/repository/PatientRepository.java
@@ -2,10 +2,25 @@ package com.example.hospitalManagement.repository;
 
 import com.example.hospitalManagement.Entity.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient,Long> {
 
     Patient findByName(String name);
+    Patient findByBirthDate(LocalDate date);
+    //expanding JPQL Methods
+    List<Patient> findByBirthDateOrEmail(LocalDate date,String Email);
+
+    List<Patient> findByNameContaining(String query);
+
+
+    List<Patient> findAllByOrderByIdDesc();
+    @Query("select * from patient where bloodgroup='O_NEGATIVE'")
+    List<Patient> findByBloodGroup();
 }
--- hospitalManagement/src/test/java/com/example/hospitalManagement/PatientTests.java
+++ hospitalManagement/src/test/java/com/example/hospitalManagement/PatientTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootTest
@@ -30,6 +31,17 @@ public class PatientTests {
         System.out.println(p1);
         Patient patient = patientRepository.findByName("Deepika Rao");
         System.out.println(patient);
+        List<Patient> patientList = patientRepository.findByBirthDateOrEmail(LocalDate.of(1990,03,14),"vinayak.desai@example.com");
+        System.out.println(patientList);
+        //ANYWHERE %'RA'$ ->
+        List<Patient> patientNameList = patientRepository.findByNameContaining("Ra");
+        System.out.println(patientNameList);
+        //DESECENDING ORDER->
+        List<Patient> patientNamedESCList = patientRepository.findAllByOrderByIdDesc();
+        System.out.println(patientNamedESCList);
+        //jpql->
+        List<Patient> patientsbloodgr = patientRepository.findByBloodGroup();
+        System.out.print(patientsbloodgr);
     }
 
 }
